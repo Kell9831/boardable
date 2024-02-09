bde75449ec4ba11d64caf742365ae68c8152d30a
Kell9831 <kellycarrion31@gmail.com>
2024-02-09 01:21:42 -0500

    "entorno router"

---
+++ src/router.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+import Login, { action as loginAction } from "./components/Login";
+import Signup, { action as signupAction } from "./components/Signup";
+import { authProvider } from "./auth";
+import App , { action as rootAction, loader as rootLoader } from "./components/App/App";
+import Error from "./components/error/error";
+import Boards from "./components/Boards";
+
+
+
+export const router = createBrowserRouter([
+  {
+    id: "app",
+    path: "/",
+    element: <App />,
+    loader: rootLoader,
+    action: rootAction,
+    error:<Error/>,
+    children: [
+      {
+        index: true,
+        element: <Boards />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    action: loginAction,
+  },
+  {
+    path: "/logout",
+    async action() {
+      await authProvider.logout();
+      return redirect("/login");
+    },
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    action: signupAction,
+    //loader: signupLoader ,
+  }
+
+]);
